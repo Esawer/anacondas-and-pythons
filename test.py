085deb6def13b8dfd40075f80baecce3b65f1fbf
Esawer <igorewease1@gmail.com>
2025-04-01 21:39:00 +0200

    This is my first commit, looking around and so on

--- test.py
+++ test.py
@@ -1,1 +1,17 @@
-print("abc")
+#Basic sorting algorithm
+
+def abc(arr: []) -> []:
+    for i in range(len(arr)):
+        checker = False
+        for j in range(len(arr)-1-i):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+                checker = True
+        if not checker:
+            return arr
+
+    return arr
+
+print(abc([3,2,6,-111,1,1]))
+
+
